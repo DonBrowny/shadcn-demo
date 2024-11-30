ad9114d5cd598cd69329d5940aed919e72beccb4
DonBrowny <kishore13ask@gmail.com>
2024-11-30 22:11:34 +0530

    minor UI changes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,16 +1,5 @@
 'use client'
-import { Calculator, Calendar, CreditCard, Settings, Smile, User } from 'lucide-react'
 import { useSession } from 'next-auth/react'
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-  CommandShortcut,
-} from '@shadcn-demo/core-ui/components/ui/command'
 import { Logout } from '../components/logout'
 
 export default function Index() {
@@ -33,51 +22,9 @@ export default function Index() {
             <span className='inline'>{`Welcome, ${session?.user.name}`}</span>
           </span>
           <Logout />
-          <h3 className='text-center text-xl font-bold leading-tight tracking-tighter md:text-3xl lg:leading-[1.1]'>
-            Nx + shadcn/ui + Next.js + Keycloak
-          </h3>
         </section>
 
-        <span className='text-center w-full inline-block mb-4'>shadcn/ui example component</span>
-
-        <Command className='rounded-lg border shadow-md h-fit'>
-          <CommandInput placeholder='Type a command or search...' />
-          <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup heading='Suggestions'>
-              <CommandItem>
-                <Calendar className='mr-2 h-4 w-4' />
-                <span>Calendar</span>
-              </CommandItem>
-              <CommandItem>
-                <Smile className='mr-2 h-4 w-4' />
-                <span>Search Emoji</span>
-              </CommandItem>
-              <CommandItem>
-                <Calculator className='mr-2 h-4 w-4' />
-                <span>Calculator</span>
-              </CommandItem>
-            </CommandGroup>
-            <CommandSeparator />
-            <CommandGroup heading='Settings'>
-              <CommandItem>
-                <User className='mr-2 h-4 w-4' />
-                <span>Profile</span>
-                <CommandShortcut>⌘P</CommandShortcut>
-              </CommandItem>
-              <CommandItem>
-                <CreditCard className='mr-2 h-4 w-4' />
-                <span>Billing</span>
-                <CommandShortcut>⌘B</CommandShortcut>
-              </CommandItem>
-              <CommandItem>
-                <Settings className='mr-2 h-4 w-4' />
-                <span>Settings</span>
-                <CommandShortcut>⌘S</CommandShortcut>
-              </CommandItem>
-            </CommandGroup>
-          </CommandList>
-        </Command>
+        <span className='text-center w-full inline-block mb-4'>Roles : {session.user.roles.toString()}</span>
       </div>
     </div>
   )
